f163f18a207c947560b11d0377424425ab8f1b3f
Adirek Laiman <2115808L@student.gla.ac.uk>
2018-03-08 14:08:57 +0000

    created basic models and added superuser

--- project/the_pantry/admin.py
+++ project/the_pantry/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from the_pantry.models import Category, Recipe
 
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Recipe)
--- project/the_pantry/models.py
+++ project/the_pantry/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+#basic models for posts and catagories created
+#need to implement user_id from authetication section
+class Category(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+
+    class Meta:
+        verbose_name_plural = "Catagories"
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=200)
+    category = models.ForeignKey(Category)
+    content = models.TextField()
+    created_on = models.DateTimeField(default=datetime.now)
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+
+    class Meta:
+        verbose_name_plural = "Recipies"
